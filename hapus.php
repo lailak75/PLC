ae58c5a30ea2d69ee185af794615196c1a2c7431
lailak75 <84852345+lailak75@users.noreply.github.com>
2021-06-14 20:07:10 +0700

    Update hapus.php

--- hapus.php
+++ hapus.php
@@ -1,6 +1,6 @@
 <?php
 include("config.php");
-if( isset($_GET['id']) ){
+if( isset($_GET['idpesanan']) ){
 	$idpesanan = $_GET['idpesanan'];
 	$query = pg_query("DELETE FROM catatan_pesanan WHERE idpesanan=$idpesanan");
 	if($query){
