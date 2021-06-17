cbacbb9740aae56fa8eb45c9b05c619e3ef1155c
lailak75 <84852345+lailak75@users.noreply.github.com>
2021-06-17 23:23:30 +0700

    Update prosespesanan.php

--- prosespesanan.php
+++ prosespesanan.php
@@ -4,19 +4,16 @@ include("config.php");
 if(isset($_POST['daftar'])){
 
 	$idpesanan = $_POST['idpesanan'];
-	$idmahasiswa = $_POST['idmahasiswa'];
-	$idusaha = $_POST['idusaha'];
 	$jb = $_POST['jenis_barang'];
 	$bb = $_POST['berat_barang'];
 	$th = $_POST['total_harga'];
 	$tm = $_POST['tanggal_masuk'];
 	$tk = $_POST['tanggal_keluar'];
-	$idkaryawan = $_POST['idkaryawan'];
-	$idadmin = $_POST['idadmin'];
-
+	$idmahasiswa = $_POST['idmahasiswa'];
+	$idusaha = $_POST['idusaha'];
 
 	// buat query
-	$query = pg_query("INSERT INTO catatan_pesanan (idpesanan, idmahasiswa, idusaha, jenis_barang, berat_barang, total_harga, tanggal_masuk, tanggal_keluar, idkaryawan, idadmin) VALUES ('$idpesanan', '$idmahasiswa', '$jb', '$bb', '$th', '$tm', '$tk', '$idkaryawan', '$idadmin')");
+	$query = pg_query("INSERT INTO catatan_pesanan (idpesanan, jenis_barang, berat_barang, total_harga, tanggal_masuk, tanggal_keluar, idmahasiswa, idusaha) VALUES ('$idpesanan', '$jb', '$bb', '$th', '$tm', '$tk', '$idmahasiswa', '$idusaha')");
 
 	// apakah query simpan berhasil?
 	if( $query==TRUE ) {
